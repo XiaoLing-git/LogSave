6c42fb5b97190009f1d4b6ea74b8e251e2b8fc13
xiaolonghua <xiaolonghua@move-new.com>
2023-09-19 10:23:20 +0800

    add new class
    1> ListLogFiles
    2> ListMnosLogFiles

---
+++ driver/list_log_files.py
@@ -0,0 +1,44 @@
+from pathlib import Path
+from typing import Union, List
+
+from driver.model import Path_Is_Not_Folder_Error
+
+
+class ListLogFiles:
+    """
+    显示指定路径下的文件名
+    """
+
+    def __init__(self, target_folder: Path):
+        self.__path = target_folder
+        self.__assert_path_is_folder()
+
+    @property
+    def path(self):
+        return self.__path
+
+    def get_all_log_files(self) -> Union[List[str], None]:
+        result = []
+        items = self.path.iterdir()
+        for item in items:
+            if item.is_file() and item.name.endswith("log"):
+                result.append(item.name)
+        if len(result) == 0:
+            return None
+        return result.copy()
+
+    def get_all_sub_folders(self) -> Union[List[str], None]:
+        result = []
+        items = self.path.iterdir()
+        for item in items:
+            if item.is_dir():
+                result.append(item.name)
+        if len(result) == 0:
+            return None
+        return result.copy()
+
+    def __assert_path_is_folder(self) -> None:
+        if not self.__path.exists():
+            raise Path_Is_Not_Folder_Error("目标路径不存在")
+        if self.__path.is_file():
+            raise Path_Is_Not_Folder_Error("目标路径不是文件夹")
---
+++ driver/list_mnos_log_files.py
@@ -0,0 +1,45 @@
+from datetime import datetime
+from pathlib import Path
+
+from driver.list_log_files import ListLogFiles
+from driver.model import Path_Is_Not_Folder_Error
+
+
+class ListMnosLogFiles(ListLogFiles):
+    """
+    获取当天Mnos文件中指定日期文件下的文件信息
+    默认是获取当天日期文件信息
+        instance= ListMnosLogFiles（）
+    也可以指定日期
+        instance= ListMnosLogFiles（_date="2023-09-05"）
+    获取所有日志名，若不存在返回None
+        instance.get_all_log_files()
+    获取已保存的所有文件夹名，若不存在返回None
+        instance.get_all_sub_folders()
+    """
+
+    def __init__(self,
+                 target_folder: Path = Path(r"/var/log/MnOS"),
+                 _date: str = None):
+        super().__init__(target_folder)
+        self.__date = _date
+        self.__path = self.__get_path()
+
+    def __get_path(self) -> Path:
+        if self.__date:
+            path = super().path / self.__date
+        else:
+            path = super().path / datetime.now().strftime("%Y-%m-%d")
+        if not path.exists():
+            raise Path_Is_Not_Folder_Error(f"目标路径<{path}>不存在")
+        if path.is_file():
+            raise Path_Is_Not_Folder_Error(f"目标路径<{path}>不是文件夹")
+        return path
+
+    @property
+    def path(self) -> Path:
+        return self.__path
+
+    @property
+    def date(self) -> str:
+        return self.__date
